0ba382b419396f4572b645ad65668e554f790dc1
Yassine Atik <brucexx02@gmail.com>
2022-12-24 23:55:41 +0100

    Updated Recent Orders Table
    I Updated Table stylings

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -6,38 +6,65 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import { color } from '@mui/system';
+import './Table.css';
 
 function createData(
     name: string,
-    calories: number,
-    fat: number,
-    carbs: number,
-    protein: number,
+    trackingId: number,
+    date: number,
+    status: number,
 ) {
-    return { name, calories, fat, carbs, protein };
+    return { name, trackingId, date, status };
 }
 
 const rows = [
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
+    createData("Lasania Chiken Fri", 1831387, "2 March 2022", "Approved"),
+    createData("Big Baza Bang", 289376, "2 March 2022", "Pending"),
+    createData("Mouth Freshner", 19203881, "2 March 2022", "Approved"),
+    createData("Cupcake", 65346289, "2 March 2022", "Delivered"),
 ];
+
+const makeStyle = (status) => {
+    if (status == 'Approved') {
+        return {
+
+            background: 'rgb(145 254 159 / 47%)',
+            padding: '10px',
+            color: 'green'
+        }
+    } else if (status == 'Pending') {
+        return {
+
+            background: '#ffadad8f',
+            color: 'red',
+            padding: '10px',
+        }
+    } else {
+        return {
+
+            background: '#59bfff',
+            padding: '10px',
+            color: 'white',
+        }
+    }
+}
 
 export default function BasicTable() {
     return (
         <div className="Table">
             <h3>Recent Orders</h3>
-            <TableContainer component={Paper}>
-                <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableContainer component={Paper}
+                style={{ boxShadow: '0px 13px 20px 0px #80808029' }}
+            >
+                <Table sx={{ minWidth: 650 }} aria-label="simple table"
+                >
                     <TableHead>
                         <TableRow>
-                            <TableCell>Dessert (100g serving)</TableCell>
-                            <TableCell align="right">Calories</TableCell>
-                            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-                            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-                            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                            <TableCell>Product</TableCell>
+                            <TableCell align="left">Tracking Id</TableCell>
+                            <TableCell align="left">Date</TableCell>
+                            <TableCell align="left">Status</TableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>
@@ -49,16 +76,18 @@ export default function BasicTable() {
                                 <TableCell component="th" scope="row">
                                     {row.name}
                                 </TableCell>
-                                <TableCell align="right">{row.calories}</TableCell>
-                                <TableCell align="right">{row.fat}</TableCell>
-                                <TableCell align="right">{row.carbs}</TableCell>
-                                <TableCell align="right">{row.protein}</TableCell>
+                                <TableCell align='left'>{row.trackingId}</TableCell>
+                                <TableCell align='left'>{row.date}</TableCell>
+                                <TableCell align='left'>
+                                    <span className='status' style={makeStyle(row.status)}>{row.status}</span>
+                                </TableCell>
+                                <TableCell align='left' className='Details'>Detail</TableCell>
                             </TableRow>
                         ))}
                     </TableBody>
                 </Table>
             </TableContainer>
-        </div>
+        </div >
 
     );
 }
